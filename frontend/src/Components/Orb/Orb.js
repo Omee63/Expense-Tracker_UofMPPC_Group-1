513bd32b950f7f5dcaf409404ed81525d713938e
Wisdom <elijahwidom6@gmail.com>
2024-02-28 09:40:01 -0600

    Make Orb less blurry and add animation to it using keyframes

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,7 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 function Orb(){
+
+    const moveOrb = keyframes`
+        0%{
+            transform: translate(0,0);
+        }
+        50%{
+            transform: translate(400px, 500px);
+        }
+        100%{
+            transform: translate(0,0);
+        }
+    `
+
 
     const OrbStyled = styled.div`
         width: 70vh;
@@ -9,9 +22,10 @@ function Orb(){
         position: absolute;
         border-radius: 50%;
         margin-left: -37vh;
-        margin-top: 37vh;
+        margin-top: -37vh;
         background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-        filter: blur(400px);
+        filter: blur(100px);
+        animation: ${moveOrb} 1s alternate linear infinite;
     `;
 
 
