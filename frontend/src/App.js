fc6958e57869ff9d5faa6abe295e16e236f2285f
Wisdom <elijahwidom6@gmail.com>
2024-02-28 09:23:45 -0600

    Create Layouts.js and test it using heading in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 // import logo from './logo.svg';
 import styled from "styled-components";
 import bg from './img/bg.png';
+import { MainLayout } from "./styles/Layouts";
+
+
 function App() {
   return (
     <AppStyled bg={bg} className="App">
-      <main>
-        
-      </main>
+        <MainLayout>
+          <h1>Hello</h1>
+        </MainLayout>
     </AppStyled>
   );
 }
+
 const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
