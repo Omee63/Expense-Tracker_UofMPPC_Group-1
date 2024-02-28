b2c3a9abf87a0d8c407a9ddb4a4a09fb841753c5
Wisdom <elijahwidom6@gmail.com>
2024-02-28 09:30:42 -0600

    Create Orb.js for floating Orb on tracker

---
+++ frontend/src/Components/Orb/Orb.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Orb(){
+
+    const OrbStyled = styled.div`
+        width: 70vh;
+        height: 70vh;
+        position: absolute;
+        border-radius: 50%;
+        margin-left: -37vh;
+        margin-top: 37vh;
+        background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
+        filer: blur(400px);
+    `;
+
+
+    return(
+        <OrbStyled></OrbStyled>
+    )
+}
+
+export default Orb
