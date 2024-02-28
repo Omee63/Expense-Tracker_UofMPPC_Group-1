371ce2c8cb850b1c0862e7d447c831f04879b230
Wisdom <elijahwidom6@gmail.com>
2024-02-28 09:16:58 -0600

    Add background and avater image to img folder and import into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
-
-
+import styled from "styled-components";
+import bg from './img/bg.png';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppStyled bg={bg} className="App">
+      <main>
+        
+      </main>
+    </AppStyled>
   );
 }
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+`;
 
 export default App;
